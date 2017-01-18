039b2623de584d8b39d28219f561915cc99a1e1b
Diorgenes <dyogenes_gd@hotmail.com>
2017-01-18 14:23:53 -0300

    Fix CDMA Devices

--- releasetools.py
+++ releasetools.py
@@ -42,9 +42,40 @@ def WritePolicyConfig(info):
   except KeyError:
     print "warning: file_context missing from target;"
 
+def CdmaConfig(info):
+  info.script.AppendExtra(("""unmount("/system");
+if getprop("ro.boot.radio") == "0x3" then
+mount("ext4", "EMMC", "/dev/block/bootdevice/by-name/system", "/system", "");
+run_program("/sbin/sh", "-c", "mv /system/etc/apns-conf.xml /system/etc/apns-conf.xml.bak");
+if getprop("ro.boot.carrier") == "sprint" then
+if getprop("ro.boot.fsg-id") == "boost" then
+run_program("/sbin/sh", "-c", "mv /system/etc/cdma/apns-conf-boost.xml /system/etc/apns-conf.xml");
+run_program("/sbin/sh", "-c", "mv /system/etc/cdma/eri-boost.xml /system/etc/eri.xml");
+endif;
+if getprop("ro.boot.fsg-id") == "frdm" then
+run_program("/sbin/sh", "-c", "mv /system/etc/cdma/apns-conf-frdm.xml /system/etc/apns-conf.xml");
+run_program("/sbin/sh", "-c", "mv /system/etc/cdma/eri-frdm.xml /system/etc/eri.xml");
+endif;
+if getprop("ro.boot.fsg-id") == "ringplus" || getprop("ro.boot.fsg-id") == "rngplus" then
+run_program("/sbin/sh", "-c", "mv /system/etc/cdma/apns-conf-rngplus.xml /system/etc/apns-conf.xml");
+run_program("/sbin/sh", "-c", "mv /system/etc/cdma/eri-rngplus.xml /system/etc/eri.xml");
+endif;
+if getprop("ro.boot.fsg-id") == "sprint" then
+run_program("/sbin/sh", "-c", "mv /system/etc/cdma/apns-conf-sprint.xml /system/etc/apns-conf.xml");
+endif;
+if getprop("ro.boot.fsg-id") == "virgin" then
+run_program("/sbin/sh", "-c", "mv /system/etc/cdma/apns-conf-virgin.xml /system/etc/apns-conf.xml");
+run_program("/sbin/sh", "-c", "mv /system/etc/cdma/eri-virgin.xml /system/etc/eri.xml");
+endif;
+endif;
+ifelse(getprop("ro.boot.carrier") == "usc", run_program("/sbin/sh", "-c", "mv /system/etc/cdma/apns-conf-usc.xml /system/etc/apns-conf.xml"));
+unmount("/system");
+endif;"""))
+
 def FullOTA_InstallEnd(info):
     WritePolicyConfig(info)
     RemoveDeviceAssert(info)
+    CdmaConfig(info)
 
 def IncrementalOTA_InstallEnd(info):
     RemoveDeviceAssert(info)
